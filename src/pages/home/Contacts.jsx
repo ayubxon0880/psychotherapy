c2103510a2416bb6fddef02efc01b35149151159
wifv <abcogenshin@gmail.com>
2025-09-22 21:16:27 +0500

    fix bugs

--- src/pages/home/Contacts.jsx
+++ src/pages/home/Contacts.jsx
@@ -8,29 +8,29 @@ const Contacts = () => {
     <div>
       <br />
       <h1 className="text-center text-3xl font-bold text-[#545453]">
-        {t("home-page.ourSpecialists.title")}
+        {t("home-page.contacts.title")}
       </h1>
       <br />
       <h1 className="text-center text-xl font-bold text-[#545453]">
-        {t("home-page.ourSpecialists.subtitle")}
+        {t("home-page.contacts.number")}
       </h1>
       <br />
       <a
-        href="tel:+998958567666"
+        href="tel:++998773786367"
         className="text-2xl bg-lime-200 hover:bg-lime-300 transition-colors px-6 py-3 rounded-lg font-medium text-gray-800 max-w-96 w-full flex justify-center m-auto"
       >
-        +998 90 123 45 67
+        +998 77 378 63 67
       </a>
       <br />
       <h1 className="text-center text-xl font-bold text-[#545453]">
-        {t("home-page.ourSpecialists.subtitle")}
+        {t("home-page.contact.email")}
       </h1>
       <br />
       <a
-        href="mailto:psychouz@gmail.com"
+        href="mailto:admin@psychotherapy.uz"
         className="text-2xl bg-lime-200 hover:bg-lime-300 transition-colors px-6 py-3 rounded-lg font-medium text-gray-800 max-w-96 w-full flex justify-center m-auto"
       >
-        psychouz@gmail.com
+        admin@psychotherapy.uz
       </a>
     </div>
   )
