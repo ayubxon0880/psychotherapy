d1b8f4d491b103d7afa63d1b61ddaf373df1135f
wifv <abcogenshin@gmail.com>
2025-09-27 01:04:19 +0500

    adding vercel analytics (not done at all)

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,20 @@ import Clinics from "./pages/Clinics.jsx";
 import Community from "./pages/Community.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Contact from "./components/Contact.jsx";
-import {TestsPage} from "./pages/TestsPage.jsx";
+import { TestsPage } from "./pages/TestsPage.jsx";
 import TestDefinition from "./pages/test/TestDefinition.jsx";
 import TestSolve from "./pages/test/TestSolve.jsx";
 import Clinic from "./pages/Clinics/Clinic.jsx";
 import Specialists from "./pages/Specialists.jsx";
 import Specialist from "./pages/Specialist.jsx";
 
+import { Analytics } from '@vercel/analytics/react';
+
 export default function App() {
     return (
         <Router>
             <Navbar />
+            <Analytics />
             <div className="pt-16">
                 <Routes>
                     <Route path="/" element={<Home />} />
@@ -31,6 +34,7 @@ export default function App() {
                     <Route path="/test/solve/:id" element={<TestSolve />} />
                 </Routes>
             </div>
+
 
             <Contact />
             <Footer />
